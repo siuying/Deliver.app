d4bf64ff468ebf72207806d438bcabdc10d74676
Francis Chong <francis@ignition.hk>
2015-07-20 10:57:08 +0800

    Initial Commit

---
+++ DeliverMetadataEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DeliverMetadataEditor
+//
+//  Created by Francis Chong on 20/7/15.
+//  Copyright © 2015 Ignition Soft. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DeliverMetadataEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DeliverMetadataEditor
+//
+//  Created by Francis Chong on 20/7/15.
+//  Copyright © 2015 Ignition Soft. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
